fbb91d3356704c6f4fe68bc953599e0d86fa5f54
rusenaite <austeja.rusenaite@gmail.com>
2022-05-05 14:31:47 +0300

    Initial commit

---
+++ Apartment.cs
@@ -0,0 +1,41 @@
+﻿namespace ApartmentArea;
+
+public class Apartment
+{
+    public Apartment Instance;
+    private readonly object _lock = new();
+
+    private volatile int Area;
+
+    public int GetArea()
+    {
+        lock (_lock)
+        {
+            return Area;
+        }
+    }
+
+    public void UpdateArea(int setValue)
+    {
+        lock (_lock)
+        {
+            Interlocked.Exchange(ref Area, setValue);
+        }
+    }
+
+    public Apartment()
+    {
+        Console.WriteLine("Apartment object " + Guid.NewGuid() + " was created.");
+    }
+    
+    public void CreateApartmentObject()
+    {
+        Instance = new Apartment();
+    }
+
+    public void ThreadProcess()
+    {
+        Console.WriteLine("New Thread started.");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ApartmentArea;
+
+Apartment sharedObj = new Apartment();
+
+var t1 = new Thread(sharedObj.ThreadProcess);
+t1.Start();
+
+Console.WriteLine(sharedObj.GetArea());
+sharedObj.UpdateArea(3);
+Console.WriteLine(sharedObj.GetArea());
+
+var t2 = new Thread(sharedObj.ThreadProcess);
+t2.Start();
+
+Console.WriteLine(sharedObj.GetArea());
+sharedObj.UpdateArea(8);
+Console.WriteLine(sharedObj.GetArea());
+
